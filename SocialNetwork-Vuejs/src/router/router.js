6a5ae90d44756dd102d6db2c48a77423a41bb76b
Ageuss <ageuoliveira0@gmail.com>
2021-09-10 10:05:53 -0300

    changes

--- SocialNetwork-Vuejs/src/router/router.js
+++ SocialNetwork-Vuejs/src/router/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home/Home.vue'
 import Login from '../views/Login/Login.vue'
 
 Vue.use(VueRouter)
@@ -10,10 +9,6 @@ const routes = [
     path: '/',
     name: 'Login',
     component: Login
-  }, {
-    path: '/Home',
-    name: 'Home',
-    component: Home
   },
 ]
 
